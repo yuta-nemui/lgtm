7024263bca0079b2ca6aa59baa134e5384e81883
n8bda1220 <n8bda1220@outlook.com>
2020-11-05 15:28:19 +0900

    'コマンドライン引数を受け取る'

--- lgtm/core.py
+++ lgtm/core.py
@@ -1,13 +1,16 @@
+from typing import Tuple
 import click
 
 
 @click.command()
-def cli():
+@click.option('--message', '-m', default='LGTM', show_default=True, help='画像に乗せる文字列')
+@click.argument('keyword')
+def cli(keyword, message):
     """LGTM画像生成ツール"""
-    lgtm()
+    lgtm(keyword, message)
     click.echo('lgtm')  # 動作確認
 
 
-def lgtm():
+def lgtm(keyword, message):
     # ここにロジックを追加していく
     pass
