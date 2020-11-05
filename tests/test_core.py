8182202bd7a00d0774fd680dbee7412e914f6ff7
n8bda1220 <n8bda1220@outlook.com>
2020-11-05 15:30:36 +0900

    'lgtm/core.pyの変更に伴うテストの修正'

--- tests/test_core.py
+++ tests/test_core.py
@@ -4,4 +4,4 @@ import unittest
 class LgtmTest(unittest.TestCase):
     def test_lgtm(self):
         form lgtm.core import lgtm
-        self.assertIsNotNone(lgtm())
+        self.assertIsNotNone(lgtm('./python.jpeg', 'LGTM'))
